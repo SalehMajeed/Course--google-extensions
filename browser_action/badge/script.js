004fc09a0571585c9b7e85df931ba67e8d622c13
= <saleh.majeed8@gmail.com>
2021-01-24 16:49:11 +0530

    badge

--- browser_action/badge/script.js
+++ browser_action/badge/script.js
@@ -1,5 +1,17 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-	chrome.browserAction.setBadgeText({ tabId: tab.id, text: 'Badge' }, () => {
-		console.log('done');
+	// set The text of the badge
+	chrome.browserAction.setBadgeText({ tabId: tab.id, text: 'color' }, () => console.log('New text has been set.'));
+
+	// get The text of the badge
+	chrome.browserAction.getBadgeText({ tabId: tab.id }, string => console.log(string));
+
+	// set The backgroundColor of the badge
+	chrome.browserAction.setBadgeBackgroundColor({ color: [22, 10, 33, 10], tabId: tab.id }, () => {
+		console.log('new color has been set');
+	});
+
+	// get The backgroundColor of the badge
+	chrome.browserAction.getBadgeBackgroundColor({ tabId: tab.id }, color_array => {
+		console.log('new color is : ' + color_array);
 	});
 });
